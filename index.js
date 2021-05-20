b437ef0a792d115e798b7caffa46f55c3c1f8a11
rtb523 <ryan.ginut@gmail.com>
2021-05-20 17:03:36 -0400

    (changed config to be in .envs)

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Discord = require('discord.js');
+require('dotenv').config();
+const config = require('./config.json');
 const client = new Discord.Client();
+const prefix = process.env.PREFIX;
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login('ODQ1MDE1MDczMDM3NzQ2MjE4.YKaznQ.84WJZm9Wzyg4L7AD_VGnEVmoBDE');
+client.login();
