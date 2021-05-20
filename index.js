11c0087f525ebd9548f7db6e39e1f261a7daabe9
rtb523 <ryan.ginut@gmail.com>
2021-05-20 17:12:26 -0400

    (added repl.it http)

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ client.once('ready', () => {
 });
 
 client.login();
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('ok');
+});
+server.listen(3000);
