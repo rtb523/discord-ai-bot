c111e3893b00390baaf5e95cc1c74d045e9a060e
rtb523 <ryan.ginut@gmail.com>
2021-05-20 16:00:04 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('ODQ1MDE1MDczMDM3NzQ2MjE4.YKaznQ.84WJZm9Wzyg4L7AD_VGnEVmoBDE');
